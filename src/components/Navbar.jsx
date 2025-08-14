f1dfaf4028fb091590626482b56a7b049715cf3b
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-08-14 16:39:03 -0700

    adding new nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,30 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <ul className="navlist">
-                <li><NavLink to="/" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Home</NavLink></li>
-                <li><NavLink to="/epoques" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Epoques</NavLink></li>
-                <li><NavLink to="/genres" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Genres</NavLink></li>
-                <li><NavLink to="/gallery" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Gallery</NavLink></li>
+
+            <ul className="menu">
+                {NAV.map((item) => (
+                    <li key={item.label}>
+                        <Link to={item.to}>{item.label}</Link>
+                        {item.children && (
+                            <ul>
+                                {item.children.map((child) => (
+                                    <li key={child.label}>
+                                        <Link to={child.to}>{child.label}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </li>
+                ))}
             </ul>
-        </nav>
+
+        </nav >
     )
 }
+
+/*<ul className="navlist">
+    <li><NavLink to="/" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Home</NavLink></li>
+    <li><NavLink to="/epoques" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Epoques</NavLink></li>
+    <li><NavLink to="/genres" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Genres</NavLink></li>
+    <li><NavLink to="/gallery" className={({ isActive }) => isActive ? "navLink active" : "navLink"}>Gallery</NavLink></li>*/
