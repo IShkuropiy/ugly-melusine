a1e7ea2392dd2be2b7ed769fa058295a1f414cce
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-08-21 11:42:40 -0700

    add baroque page and css

--- src/pages/epoques/Baroque.jsx
+++ src/pages/epoques/Baroque.jsx
@@ -1,29 +1,25 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+//import { Link } from 'react-router-dom';
 //import { NavLink } from "react-router-dom";
-import './Epoques.css';
-import './epoques/Baroque'
-import '../assets/laMeninas.jpg'
-export default function Epoques() {
+import './Baroque.css';
+
+export default function Baroque() {
+
 
     return (
-        <div className="e-content">
-            <div className="epoques-hero">
-                <div className="epoques-hero-overlay">
-                    <h1>Baroque Era Music Guide</h1>
-                </div>
+        <div className="baroque-content">
+
+            <div className="baroque-hero">
+                <h1>Baroque Era Music Guide</h1>
             </div>
 
 
-            <div className="epoques-content">
-                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, adipisci.</h2>
+            <div className="baroque-content">
+                <h2>What is Baroque Music?</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repudiandae eos aliquam ratione ullam iusto accusantium minus eaque, laudantium odio rem modi ea impedit omnis amet sunt error magnam blanditiis placeat voluptatum et ipsa commodi repellat! Voluptate fugiat asperiores ad rem voluptatibus fuga, sunt perferendis inventore possimus error laudantium esse assumenda distinctio officiis hic quo eius eaque labore dicta? Sapiente, temporibus consequatur, iste dolores ipsum iure hic illo natus dolorum aliquam commodi quia nesciunt autem, ex officiis incidunt cum deserunt. Hic laborum molestias quia reprehenderit placeat eveniet perferendis assumenda ipsam autem nostrum, aliquam necessitatibus ducimus officia vero nemo iusto cum, mollitia quo quidem exercitationem suscipit. Obcaecati earum perferendis sint, facere cum nulla sit alias sapiente veritatis, odit id repellat? Qui ut neque laboriosam possimus quisquam beatae, nobis quaerat repudiandae veritatis similique necessitatibus minus iste eaque amet officiis voluptatum soluta eos adipisci, ex dolorum natus. Enim blanditiis porro molestias? Impedit, ipsa eveniet. Amet ullam nesciunt, ea error unde molestias aperiam quaerat nostrum, ex, vitae iste reprehenderit? Ipsa sapiente eveniet impedit illum cumque voluptatibus voluptatum quos, perferendis voluptas dolorum eum et. Delectus aliquam ullam ad tempora ut voluptate nisi doloremque error commodi velit. Aspernatur delectus quam perferendis, ut harum repellendus laborum nam?
                 </p>
             </div>
-
         </div>
-
-
-    )
-}
+    );
+} 
