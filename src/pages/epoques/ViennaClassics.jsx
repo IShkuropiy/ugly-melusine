7ef5d33d13a137581d4dd719c4cb5bdb3d5a28f1
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-08-26 18:46:59 -0700

    add new vienna page

--- src/pages/epoques/ViennaClassics.jsx
+++ src/pages/epoques/ViennaClassics.jsx
@@ -4,18 +4,18 @@ import React from 'react';
 import './Baroque.css';
 import '../assets/familyBaroque.jpg'
 
-export default function Baroque() {
+export default function  ViennaClassics() {
 
     return (
-        <div className="b-container">
-            <div className="b-hero">
-                <div className="b-hero-overlay">
+        <div className="v-container">
+            <div className="v-hero">
+                <div className="v-hero-overlay">
                     <h1>Baroque Era Music Guide</h1>
                 </div>
             </div>
 
 
-            <div className="b-content">
+            <div className="v-content">
                 <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, adipisci.</h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores repudiandae eos aliquam ratione ullam iusto accusantium minus eaque, laudantium odio rem modi ea impedit omnis amet sunt error magnam blanditiis placeat voluptatum et ipsa commodi repellat! Voluptate fugiat asperiores ad rem voluptatibus fuga, sunt perferendis inventore possimus error laudantium esse assumenda distinctio officiis hic quo eius eaque labore dicta? Sapiente, temporibus consequatur, iste dolores ipsum iure hic illo natus dolorum aliquam commodi quia nesciunt autem, ex officiis incidunt cum deserunt. Hic laborum molestias quia reprehenderit placeat eveniet perferendis assumenda ipsam autem nostrum, aliquam necessitatibus ducimus officia vero nemo iusto cum, mollitia quo quidem exercitationem suscipit. Obcaecati earum perferendis sint, facere cum nulla sit alias sapiente veritatis, odit id repellat? Qui ut neque laboriosam possimus quisquam beatae, nobis quaerat repudiandae veritatis similique necessitatibus minus iste eaque amet officiis voluptatum soluta eos adipisci, ex dolorum natus. Enim blanditiis porro molestias? Impedit, ipsa eveniet. Amet ullam nesciunt, ea error unde molestias aperiam quaerat nostrum, ex, vitae iste reprehenderit? Ipsa sapiente eveniet impedit illum cumque voluptatibus voluptatum quos, perferendis voluptas dolorum eum et. Delectus aliquam ullam ad tempora ut voluptate nisi doloremque error commodi velit. Aspernatur delectus quam perferendis, ut harum repellendus laborum nam?
